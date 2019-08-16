9b437209b10217de934bd57d9c73f8a675adc84b
Kapil Verma <kapil.verma@crediton.com>
2019-08-16 16:07:20 +0530

    AuthController in works

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,25 +4,69 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Validator;
+use Hash;
 
 class AuthController extends Controller {
     public function register(Request $request)
     {
-        // todo
-        $user = new User()->fill($request->input());
+        $input = $request->input();
+
+        $validator = Validator::make($input, [
+            'name' => 'required|max:191',
+            'email' => 'required|unique:users,email|email|max:191',
+            'password' => 'required|min:6|max:191'
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json($validator->errors(), 400);
+        }
+
+        // salted password encryption before saving
+        $input['password'] = Hash::make($input['password']);
+
+        $user = (new User())->fill($input);
         $user->save();
 
+        // $token = AuthService::loginUserAndGetToken($user);
+
         return response()->json([
-            'action' => 'register'
+            'msg' => 'registered'
         ]);
     }
 
-    public function login()
+    public function login(Request $request)
     {
-        // todo
-        return response()->json([
-            'action' => 'login'
+        $input = $request->input();
+
+        $validator = Validator::make($input, [
+            'email' => 'required|email|max:191|exists:users',
+            'password' => 'required|min:6|max:191'
         ]);
+
+        if ($validator->fails())
+        {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::where('email', $input['email'])->first();
+        $success = Hash::check($input['password'], $user->password);
+
+        if ($success)
+        {
+            // $token = AuthService::loginUserAndGetToken($user);
+
+            return response()->json([
+                'msg' => 'logged in'
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'msg' => 'invalid email/password'
+            ], 400);
+        }
     }
 
     public function account()
